1c964bdf26be9d262be13610cd51c9efd480a830
Felix.Xu <xxfstronger@gmail.com>
2018-05-26 13:29:21 +0100

    Update JDK version

--- java-base/src/main/java/com/felix/learning/java/base/Helloworld.java
+++ java-base/src/main/java/com/felix/learning/java/base/Helloworld.java
@@ -1,7 +1,11 @@
 package main.java.com.felix.learning.java.base;
 
+import java.util.ArrayList;
+
 public class Helloworld {
     public static void main(String[] args){
+        var a = new ArrayList<>();
+
         System.out.println("Hello World");
 
     }
